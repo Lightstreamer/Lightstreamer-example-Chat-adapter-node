3ae450b840eaa17be72dd10885c3ae5409aafd0a
mone <simone.fabiano@gmail.com>
2013-03-28 17:43:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,58 @@
+# Lightstreamer Chat Demo adapter for Node #
+
+This project includes an example Lightstreamer remote chat adapter to be deployed on a node instance.
+It shows the use of DataProvider and MetadataProvider classes provided in the lightstreamer-adapter node module.
+
+## Install the example ##
+The following steps assume that Lightstreamer Server, Adapter and Client are launched on the same machine. Adapt hosts accordingly when separating the pieces across different machines.
+
+### Prepare Lightstreamer ###
+
+1.    Download and install Lightstreamer
+2.    Go to the "adapters" folder of your Lightstreamer Server installation. Create a new folder and call it "ChatAdapterNode". Create a "lib" folder inside it.
+3.    Copy the "ls-proxy-adapters.jar" file from "Lightstreamer/DOCS-SDKs/sdk_adapter_remoting_infrastructure/lib" in the newly created "lib" folder.
+4.    Create an "adapters.xml" file inside the "NodeAdapter" folder and use the following contents:
+```xml      
+<?xml version="1.0"?>
+
+<adapters_conf id="PROXY_NODECHAT">
+
+  <metadata_provider>
+    <adapter_class>com.lightstreamer.adapters.remote.metadata.RobustNetworkedMetadataProvider</adapter_class>
+    <param name="request_reply_port">8003</param>
+    <param name="timeout">36000000</param>
+  </metadata_provider>
+
+  <data_provider name="CHAT_ROOM">
+    <adapter_class>com.lightstreamer.adapters.remote.data.RobustNetworkedDataProvider</adapter_class>
+    <param name="request_reply_port">8001</param>
+    <param name="notify_port">8002</param>
+    <param name="timeout">36000000</param>
+  </data_provider>
+
+</adapters_conf>
+```
+
+5.    Launch Lightstreamer.
+
+### Prepare the Remote Adapter ###
+
+1.    From the command line go to the folder of this project and call
+```
+npm install lightstreamer-adapter
+```
+
+2.    Run the adapter using
+```
+node nodechat.js
+```
+
+### Prepare the Client ###
+
+1.    Copy the "Lightstreamer/pages/demos/ChatDemo" folder from the Lightstreamer distribution
+2.    Edit the index.html file: search the LightstreamerClient instantiation and replace the "DEMO" string with "PROXY_NODECHAT"
+```js
+new LightstreamerClient(hostToUse,"PROXY_NODECHAT");
+```
+
+3.    Open the index.html file in one or more browsers. You can either launch the file using the file:/// protocol (i.e.: by double-clicking the file on most systems) or by deploying the folder on a local webserver (you may use Lightstreamer internal webserver).
---
+++ nodechat.js
@@ -0,0 +1,105 @@
+var 
+  // Imports
+  MetadataProvider = require('lightstreamer-adapter').MetadataProvider,
+  DataProvider = require('lightstreamer-adapter').DataProvider,
+  net = require('net'),
+  inspect = require('util').inspect,
+
+  // Remote proxy host and ports
+  HOST = 'localhost',
+  REQ_RESP_PORT = 8001,
+  WRITE_PORT = 8002,
+  META_PORT = 8003,
+
+  // Request/response socket channel
+  reqRespStream,
+
+  // Push data socket channel
+  notifyStream,
+
+  // Metadata socket channel
+  metadataStream,
+
+  // The data provider object
+  dataProvider,
+
+  // The data provider object
+  metadataProvider,
+
+  // JS timer for the simulated data push 
+  subscribed = false,
+
+  // User data just for the demo
+  sessions = [];
+
+// Create socket connections
+reqRespStream = net.createConnection(REQ_RESP_PORT, HOST);
+notifyStream = net.createConnection(WRITE_PORT, HOST);
+metadataStream = net.createConnection(META_PORT, HOST);
+
+// Create the data provider object from the lightstreamer module
+dataProvider = new DataProvider(reqRespStream, notifyStream);
+
+// Create the metadata provider object from the lightstreamer module
+metadataProvider = new MetadataProvider(metadataStream, {
+  distinctSnapLen: 30,
+  itemAllowedModes: {distinct: true},
+  userAllowedModes: {distinct: true},
+});
+
+// Handle subscribe event
+dataProvider.on('subscribe', function(itemName, response) {
+  console.log("Subcribed item: " + itemName);
+  if (itemName === "chat_room") {
+    subscribed = true;
+    response.success();    
+  } else {
+    response.error("No such item");
+  }
+});
+
+// Handle unsubscribe event
+dataProvider.on('unsubscribe', function(itemName, response) {
+  console.log("Unsubscribed item: " + itemName);
+  if (itemName === "chat_room") {
+    subscribed = false;
+    response.success();
+  } else {
+    response.error("No such item");
+  }
+});
+
+// Handle new session event and store user session data
+metadataProvider.on('notifyNewSession', function(request, response) {
+  console.log("New session: " + inspect(request));
+  sessions[request.sessionId] = request.contextProperties;
+  response.success();    
+});
+
+// Handle close session event and remove stored session data
+metadataProvider.on('notifySessionClose', function(request, response) {
+  console.log("Close session: " + inspect(request));
+  delete(sessions[request.sessionId]);
+  response.success();    
+});
+
+// Handle and incoming message and push it to the chat_room item
+metadataProvider.on('notifyUserMessage', function(request, response) {
+  console.log("New user message: " + inspect(request));
+  if (!subscribed) {
+    response.error("Unexpected message");
+  }
+  if (!sessions[request.sessionId]) {
+    response.error("Session lost! Please reload the browser page(s).");
+  }
+  var session = sessions[request.sessionId];
+  var userMessage = request.userMessage.split("|")[1];
+  dataProvider.update("chat_room", false, {
+    'timestamp': new Date().getTime() + '',
+    'IP': session.REMOTE_IP,
+    'nick': session.USER_AGENT,
+    'message': userMessage
+  });
+  response.success();
+});
+
